0388e0c71c3fc41b0c9471443ba082ec0b9eef8d
Vamsee Achanta <achantav@gmail.com>
2025-02-15 22:15:44 -0600

    20250215

--- dev_tools/au_git_sync/au_git_daily_commit_all_repos.sh
+++ dev_tools/au_git_sync/au_git_daily_commit_all_repos.sh
@@ -1,20 +1,26 @@
 # shell script to perform daily git operations - all repos
 au_git_sync_home=$(pwd)
+au_daily_commit_script="${au_git_sync_home}/au_git_daily_commit.sh"
 
 # source common utilities
 if [ ! -f "${au_git_sync_home}/common.sh" ]; then
     source ${au_git_sync_home}/common.sh
 fi
 
-au_daily_commit_script="${au_git_sync_home}/au_git_daily_commit.sh"
-
-# get to repo root
-repo_root=$(git rev-parse --show-toplevel)
-cd "$repo_root"
-repo_name=$(basename $(git rev-parse --show-toplevel))
-
-# Directory containing GitHub repositories
-github_dir=$(dirname "$(pwd)")
+# Check if current directory is a git repository
+if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+    # get to repo root
+    repo_root=$(git rev-parse --show-toplevel)
+    cd "$repo_root"
+    repo_name=$(basename $(git rev-parse --show-toplevel))
+    # Directory containing GitHub repositories
+    github_dir=$(dirname "$(pwd)")
+else
+    cd ../../
+    github_dir=$(dirname "$(pwd)")
+    log_message "yellow" "$(pwd) is not a git repository. Define a valid directory to run this script."
+    exit 1
+fi
 
 cd ${github_dir}
 log_message "normal" "Starting repository check-in routine process in $(pwd)..."
