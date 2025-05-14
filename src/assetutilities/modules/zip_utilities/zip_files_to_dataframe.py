1c83eeabf7e4d8bca95ab18a41365b3a0de47185
samdansk2 <samdanshaik8998@gmail.com>
2025-05-14 11:28:46 +0530

    wip

--- src/assetutilities/modules/zip_utilities/zip_files_to_dataframe.py
+++ src/assetutilities/modules/zip_utilities/zip_files_to_dataframe.py
@@ -16,12 +16,7 @@ class ZipFilestoDf:
     
     def zip_file_to_dataframe(self, cfg) -> Dict[str, pd.DataFrame]:
 
-        """
-        Extracts files from a ZIP file/archive and returns Pandas DataFrame(s) as dictionary.
-
-        Returns:
-         - A dictionary where keys are filenames and values are Pandas DataFrames if multiple files exist.
-        """
+        """ Extracts files from a ZIP file/archive and returns Pandas DataFrame(s) as dictionary."""
 
         column_names = cfg['zip_utilities']['column_names']
         zip_file_name_with_path = cfg['zip_utilities']['file_name']
@@ -45,24 +40,28 @@ class ZipFilestoDf:
 
             dataframe_dict = {}
             delimiter = ','
-            for file_to_read in file_list:
-                with zf.open(file_to_read) as file:
+            dataframe_dict = self.parse_zip_files(column_names, rows, zf, file_list, dataframe_dict, delimiter)
+
+        return dataframe_dict
+
+    def parse_zip_files(self, column_names, rows, zf, file_list, dataframe_dict, delimiter):
+        
+        for file_to_read in file_list:
+            with zf.open(file_to_read) as file:
+                try:
+                    df = pd.read_csv(file, sep=delimiter, on_bad_lines='warn',low_memory=False,nrows=rows)
+                except Exception as e:
                     try:
                         df = pd.read_csv(file, sep=delimiter, encoding='latin-1', on_bad_lines='warn',low_memory=False,nrows=rows)
-                    # except Exception as e:
-                    #     try:
-                    #         df = pd.read_csv(file, sep=delimiter, encoding='latin-1', on_bad_lines='warn',low_memory=False,nrows=rows)
                     except Exception as e:
                         logger.error(f"Error reading file '{file_to_read}': {e}")
                         continue
+  
+                # If column names are provided and the df has no header set the column names
+                if column_names: 
+                    df.columns = column_names
 
-                    # If column names are provided and the df has no header set the column names
-                    if column_names: 
-                        df.columns = column_names
-
-                    file_basename = os.path.basename(file_to_read)
-                    file_name_without_extension, extension = os.path.splitext(file_basename)
-                    dataframe_dict[file_name_without_extension] = df
-
-        return dataframe_dict
+                file_basename = os.path.basename(file_to_read)
+                file_name_without_extension, extension = os.path.splitext(file_basename)
+                dataframe_dict[file_name_without_extension] = df
 
