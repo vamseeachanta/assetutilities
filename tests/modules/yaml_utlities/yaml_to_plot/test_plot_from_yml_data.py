e5a5eb595dcee9aef224c7c60ec97fa8b5ac37da
Vamsee Achanta <achantav@gmail.com>
2025-07-13 23:26:30 -0500

    wip

--- tests/modules/yaml_utlities/yaml_to_plot/test_plot_from_yml_data.py
+++ tests/modules/yaml_utlities/yaml_to_plot/test_plot_from_yml_data.py
@@ -1,35 +1,16 @@
-# Standard library imports
+test_run_process()
 import os
-import sys
-
-# Reader imports
-from assetutilities.engine import engine
+from src.assetutilities.engine import engine
 
 
-def run_process(input_file, expected_result={}):
+def run_process(input_file):
     if input_file is not None and not os.path.isfile(input_file):
         input_file = os.path.join(os.path.dirname(__file__), input_file)
-    cfg = engine(input_file)
-
-def get_valid_pytest_output_file(pytest_output_file):
-    if pytest_output_file is not None and not os.path.isfile(pytest_output_file):
-        pytest_output_file = os.path.join(os.path.dirname(__file__), pytest_output_file)
-    return pytest_output_file
+    result = engine(input_file)
+    return result
 
 
 def test_run_process():
-    # input_file = "test_plot_from_yml_data1.yml"
     input_file = "test_plot_from_yml_example_data1.yml"
-    
-    # pytest_output_file = '../test_data/6d_buoy/buoy_6d_circular_px_0_pytest.yml'
-    # pytest_output_file = get_valid_pytest_output_file(pytest_output_file)
-    expected_result = {}
-    # expected_result = ymlInput(pytest_output_file, updateYml=None)
-
-    if len(sys.argv) > 1:
-        sys.argv.pop()
-
-    run_process(input_file, expected_result)
-
-
-test_run_process()
+    result = run_process(input_file)
+    assert result is not None
