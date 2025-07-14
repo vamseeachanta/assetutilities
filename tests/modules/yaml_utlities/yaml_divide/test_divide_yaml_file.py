b92977ed224488d3db1c085bd5e27cb9fab82529
Vamsee Achanta <achantav@gmail.com>
2025-07-13 23:09:28 -0500

    wip

--- tests/modules/yaml_utlities/yaml_divide/test_divide_yaml_file.py
+++ tests/modules/yaml_utlities/yaml_divide/test_divide_yaml_file.py
@@ -1,43 +1,18 @@
-# Standard library imports
 import os
-import sys
-import deepdiff
+from src.assetutilities.engine import engine
 
-import colorama
-from assetutilities.common.yml_utilities import ymlInput #noqa
 
-# Reader imports
-from assetutilities.engine import engine #noqa
-from assetutilities.modules.test_utilities.test_utilities import TestUtilities
-
-colorama.init(autoreset=True)
-
-tu = TestUtilities()
-
-def run_process(input_file, expected_result):
+def run_process(input_file):
     if input_file is not None and not os.path.isfile(input_file):
         input_file = os.path.join(os.path.dirname(__file__), input_file)
-    cfg = engine(input_file)
-
-    # obtained_result = cfg[cfg['basename']]['divide']
-    # expected_result = expected_result[cfg['basename']]['divide'].copy()
-
-    # for group_index, group_ele in enumerate(obtained_result['groups']):
-    #     for data_index, data_ele in enumerate(group_ele[group_index]):
-    #         file_obtained = obtained_result['groups'][group_index][data_index]['data']
-    #         file_expected = expected_result['groups'][group_index][data_index]['data']
-
-    #         file_obtained_yml = ymlInput(file_obtained)
-    #         file_expected_yml = ymlInput(file_expected)
-            
-    #         assert not deepdiff.DeepDiff(
-    #             file_obtained_yml, file_expected_yml, ignore_order=True, significant_digits=4
-    #         )
-
-    return cfg
+    result = engine(input_file)
+    return result
 
 
 def test_run_process():
+    input_file = "divide_yaml_file.yml"
+    result = run_process(input_file)
+    assert result is not None
     input_file = "divide_yaml_file_by_primary_key.yml"
 
     # pytest_output_file = "results/divide_yaml_file_by_primary_key_pytest.yml"
